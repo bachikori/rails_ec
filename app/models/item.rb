e86ffcc1f251d9692d619a6db612ffc7f2a62c6a
bachikori <ya.7117.goo@gmail.com>
2023-02-19 11:39:40 +0900

    validation追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,7 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   validates :stock, presence: true
+  validates :image_name, presence: true
+  validates :review, presence: true
+  validates :sale, presence: true
 end
