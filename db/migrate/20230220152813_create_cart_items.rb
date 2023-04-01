288be7f0afb678a4420ee9e253bd2e8bd544b24a
bachikori <ya.7117.goo@gmail.com>
2023-04-01 20:56:59 +0900

    migrationエラーを修正

--- db/migrate/20230220152813_create_cart_items.rb
+++ db/migrate/20230220152813_create_cart_items.rb
@@ -3,7 +3,6 @@
 class CreateCartItems < ActiveRecord::Migration[7.0]
   def change
     create_table :cart_items do |t|
-      t.integer :item_id, null: false
       t.integer :quantity, null: false, default: 1
 
       t.timestamps
